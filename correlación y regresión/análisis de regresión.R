f83166804e6def049164294a875503011e81c773
ByronG <byrong.gt@gmail.com>
2023-08-11 17:40:40 -0600

    Actualizado el modelo de regresión

--- correlación y regresión/análisis de regresión.R
+++ correlación y regresión/análisis de regresión.R
@@ -8,14 +8,17 @@ if(!require(readxl)){install.packages("readxl")}
 
 salinidad<-read_excel("salinidad1.xlsx")
 names(salinidad)
+head(salinidad)
 cor(salinidad)
 
 # Modelo para explicar el magnesio
 attach(salinidad)
 plot(ce,mg)
 model1<-lm(mg~ce, data=salinidad)
+abline(model1, col="red", lwd=3)
 salinidad$pred_mg<-model1$fitted.values
 salinidad$res_mg<-model1$residuals
+head(salinidad)
 summary(model1)
 windows(10,10)
 check_model(model1)
@@ -23,6 +26,7 @@ check_normality(model1)
 
 plot(log(ce),log(mg))
 model2<-lm(log(mg)~log(ce))
+abline(model2, col="blue", lwd=3)
 summary(model2)
 windows(10,10)
 check_model(model2)
