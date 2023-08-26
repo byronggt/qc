170c93debd250be0d2977f7339d137a57c70263d
ByronG <byrong.gt@gmail.com>
2023-08-26 15:22:23 -0600

    Agregado resumen de capacidad del proceso para fenitoína

---
+++ capacidad del proceso/resumen de capacidad del proceso para fenitoína.R
@@ -0,0 +1,19 @@
+# Dr. Byron González
+# http://www.byrong.tk 
+
+if(!require(readxl)){install.packages("readxl")}
+if(!require(qcc)){install.packages("qcc")}
+
+# Importar la tabla de datos "fenpos" 
+
+fenit<-read_excel("fenpos.xlsx")
+attach(fenit)
+fenit1 <- qcc.groups(pesos,muestra)
+
+# Gráficos de control
+obj <- qcc(fenit1, type="xbar") # Gráfico de media
+obj1 <- qcc(fenit1, type="R") # Gráfico de rangos
+obj2 <- qcc(fenit1, type="S") # Gráfico de desviación estándar 
+
+# Capacidad del proceso
+process.capability(obj, spec.limits=c(423,468))  
