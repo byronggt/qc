b47fbfc68e9c93a2a4bc4a2bcad8f4283bcca4c4
ByronG <byrong.gt@gmail.com>
2024-08-04 13:22:06 -0600

    Actualizado el análisis de regresión

--- correlación y regresión/análisis de regresión.R
+++ correlación y regresión/análisis de regresión.R
@@ -1,6 +1,5 @@
 # Dr. Byron González
-# Dr. Ezequiel López
-# http://cete.fausac.gt
+# http://byrong.cc
 
 if(!require(performance)){install.packages("performance")}
 if(!require(jtools)){install.packages("jtools")}
