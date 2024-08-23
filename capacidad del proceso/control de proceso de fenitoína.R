4c336c198ad390314ab53acdd62f7f7270ce832a
ByronG <byrong.gt@gmail.com>
2024-08-22 18:56:53 -0600

    Actualizado control del proceso para fenitoína

--- capacidad del proceso/control de proceso de fenitoína.R
+++ capacidad del proceso/control de proceso de fenitoína.R
@@ -1,6 +1,5 @@
 # Dr. Byron González
-# Dr. Ezequiel López
-# http://cete.fausac.gt
+# http://byrong.cc
 # IMPORTANTE: Emplear desde el inicio 
 # la versión de R 3.6.3 para el paquete qualityTools
 
